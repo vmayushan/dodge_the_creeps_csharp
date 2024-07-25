51c640c26f2ea8676dbad6ac074095ba9ac923c3
Vlad Ma-iu-shan <Vladislav.Ma-iu-shan@jetbrains.com>
2024-07-25 13:18:34 +0200

    Make game work on 4.2

--- Mob.cs
+++ Mob.cs
@@ -2,15 +2,15 @@ using Godot;
 
 public partial class Mob : RigidBody2D
 {
-    public override void _Ready()
-    {
-        var animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-        string[] mobTypes = animatedSprite.SpriteFrames.GetAnimationNames();
-        animatedSprite.Play(mobTypes[GD.Randi() % mobTypes.Length]);
-    }
+	public override void _Ready()
+	{
+		var animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		string[] mobTypes = animatedSprite.SpriteFrames.GetAnimationNames();
+		animatedSprite.Play(mobTypes[GD.Randi() % mobTypes.Length]);
+	}
 
-    public void OnVisibleOnScreenNotifier2DScreenExited()
-    {
-        QueueFree();
-    }
+	public void OnVisibleOnScreenNotifier2DScreenExited()
+	{
+		QueueFree();
+	}
 }

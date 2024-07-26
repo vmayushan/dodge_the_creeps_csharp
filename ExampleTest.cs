97c0a7009e6bb05234e51e3e45fbaee7d0dbd0e6
Vlad Ma-iu-shan <Vladislav.Ma-iu-shan@jetbrains.com>
2024-07-26 14:50:02 +0200

    add .runsettings

--- ExampleTest.cs
+++ ExampleTest.cs
@@ -6,15 +6,15 @@ using static GdUnit4.Assertions;
 [TestSuite]
 public class ExampleTest
 {
-    [TestCase]
-    public void success()
-    {
-        AssertBool(true).IsTrue();
-    }
-    
-    [TestCase]
-    public void failed()
-    {
-        AssertBool(false).IsTrue();
-    }
+	[TestCase]
+	public void success()
+	{
+		AssertBool(true).IsTrue();
+	}
+	
+	[TestCase]
+	public void failed()
+	{
+		AssertBool(false).IsTrue();
+	}
 }

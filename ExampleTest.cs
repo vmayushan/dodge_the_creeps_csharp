0bb12550e9bf2591a931f7f46143d86919ab6b3d
Vlad Ma-iu-shan <Vladislav.Ma-iu-shan@jetbrains.com>
2024-07-26 13:21:05 +0200

    add gdunit4 to the project

---
+++ ExampleTest.cs
@@ -0,0 +1,20 @@
+namespace DodgeTheCreeps;
+
+using GdUnit4;
+using static GdUnit4.Assertions;
+
+[TestSuite]
+public class ExampleTest
+{
+    [TestCase]
+    public void success()
+    {
+        AssertBool(true).IsTrue();
+    }
+    
+    [TestCase]
+    public void failed()
+    {
+        AssertBool(false).IsTrue();
+    }
+}
